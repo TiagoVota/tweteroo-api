e413f3fd13e97463fbe95c8688b6e95515ec3e72
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2022-01-24 10:22:00 -0500

    Make get tweets end point

--- src/services/tweetsService.js
+++ src/services/tweetsService.js
@@ -1,14 +1,21 @@
-const sendTweet = () => {
+const tweetsList = []
 
+
+const getTweets = (req, res) => {
+	const lastTweets = 	takeLastElements(tweetsList, 10)
+
+	return res.status(200).send(lastTweets)
 }
 
+const takeLastElements = (arr, qnt) => arr.slice(Math.max(arr.length - qnt, 0))
 
-const getTweets = () => {
+
+const sendTweet = (req, res) => {
 
 }
 
 
 export {
-	sendTweet,
 	getTweets,
+	sendTweet,
 }
