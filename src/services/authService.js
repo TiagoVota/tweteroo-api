0452edc89920e5845fa3b9e3a3154b56981b9aa7
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2022-01-24 10:10:49 -0500

    Make sign-up end point

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,21 @@
-const signUp = () => {
+const accountsList = []
 
+
+const signUp = (req, res) => {
+	const { username, avatar } = req.body
+
+	addUser({ username, avatar })
+
+	console.log({accountsList})
+
+	return res.status(201).send('Usuário criado com sucesso!')
+}
+
+const addUser = ({ username, avatar }) => {
+	accountsList.push({
+		username,
+		avatar			
+	})
 }
 
 
